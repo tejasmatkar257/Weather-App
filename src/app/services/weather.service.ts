ae5211ebea42930dd0305b57a69844d3fd87daae
Tejas <tejasmatkar257@gmail.com>
2023-08-08 13:36:00 +0530

    2nd

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { WeatherData } from '../models/weather.model';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+    getWeatherData(cityName: string): Observable<WeatherData> {
+      return this.http.get<WeatherData>(environment.weatherApiBaseUrl+"?", {
+        headers: new HttpHeaders()
+        .set(environment.XRapidAPIHostHeaderName , environment.XRapidAPIHostHeaderValue )
+        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+        params: new HttpParams()
+        .set('q',cityName)
+        .set('appid','da0f9c8d90bde7e619c3ec47766a42f4')
+        .set('appid','da0f9c8d90bde7e619c3ec47766a42f4')
+        .set('units','metric')
+        .set('mode','json')
+      })
+    }
+   }
+
+
+
+
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    weatherApiBaseUrl:'https://openweather43.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'openweather43.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'b8ba27b2c0msh175b447d329d04dp146a9ejsnda68b2042c9b'
+};
